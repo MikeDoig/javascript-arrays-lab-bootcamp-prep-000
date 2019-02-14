cfc86ce7d752c7876c834e00acdc930c7c158abb
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 18:01:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,26 +2,18 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 var destructivelyAppendKitten = function(array){
-  kittens.push(array);
 };
-var destructivelyPrependKitten = function (array) {
-  kittens.unshift(array);
+var destructivelyPrependKitten = function () {
 };
-var destructivelyRemoveLastKitten = function(array) {
-  kittens.pop();
+var destructivelyRemoveLastKitten = function() {
 };
-var destructivelyRemoveFirstKitten = function(array) {
-  kittens.shift();
+var destructivelyRemoveFirstKitten = function() {
 };
-var appendKitten = function(array) {
-  return [...kittens,array];
+var appendKitten = function() {
 };
-var prependKitten = function(array ) {
-  return [array,...kittens];
+var prependKitten = function( ) {
 };
 var removeLastKitten = function() {
-  return kittens.slice(0, kittens.length -1);
 };
-var removeFirstKitten = function(array) {
-  return kittens.slice(1);
+var removeFirstKitten = function() {
 };
