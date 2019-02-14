8ebd0922d036b95b716da213c2ccdf11127bc4b9
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 18:03:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 var destructivelyAppendKitten = function(array){
-  array.push(array);
+  kittens.push(array);
   return array;
 };
 var destructivelyPrependKitten = function () {
